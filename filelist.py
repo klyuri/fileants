b9136cf8f30851ebdd296f335c3570b7fcef7d3f
klyuri <klyurival@gmail.com>
2021-02-25 00:45:42 +0300

    learn: Add filelist.py

---
+++ filelist.py
@@ -0,0 +1,13 @@
+import os
+
+start_dir="test_data/hill1"
+
+print(f"HILL '{start_dir}' :")
+
+for base_dir,dirs,files in os.walk(start_dir):
+    for file_name in files:
+        name = os.path.join(base_dir,file_name)
+        print(f"'{name}'")
+
+#Run:
+#  python ./filelist.py

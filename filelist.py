5b79b38178536a6f1f670965a8914d2c3e9d0173
klyuri <klyurival@gmail.com>
2021-02-25 01:01:24 +0300

    learn:(filelist) Add "__name__ == '__main__'"

--- filelist.py
+++ filelist.py
@@ -2,14 +2,14 @@
 
 import os
 
-start_dir="test_data/hill1"
+if __name__ == '__main__':
 
-print(f"HILL '{start_dir}' :")
-
-for base_dir,dirs,files in os.walk(start_dir):
-    for file_name in files:
-        name = os.path.join(base_dir,file_name)
-        print(f"'{name}'")
+    start_dir="test_data/hill1"
+    print(f"HILL '{start_dir}' :")
+    for base_dir,dirs,files in os.walk(start_dir):
+        for file_name in files:
+            name = os.path.join(base_dir,file_name)
+            print(f"'{name}'")
 
 #Run:
 #  ./filelist.py

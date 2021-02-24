3f96eec8eb6606f3b282e5781b55e9807568bfed
klyuri <klyurival@gmail.com>
2021-02-25 01:04:40 +0300

    learn:(filelist) Create print_hill()

--- filelist.py
+++ filelist.py
@@ -2,14 +2,17 @@
 
 import os
 
-if __name__ == '__main__':
-
-    start_dir="test_data/hill1"
+def print_hill(start_dir):
     print(f"HILL '{start_dir}' :")
     for base_dir,dirs,files in os.walk(start_dir):
         for file_name in files:
             name = os.path.join(base_dir,file_name)
             print(f"'{name}'")
+
+if __name__ == '__main__':
+
+    start_dir="test_data/hill1"
+    print_hill(start_dir)
 
 #Run:
 #  ./filelist.py

21b8f15e0fa3d74bdc5299d4b12e8d42b39114a4
klyuri <klyurival@gmail.com>
2021-02-27 15:27:17 +0300

    learn:(fileants) Used ant_arg

--- fileants.py
+++ fileants.py
@@ -1,33 +1,53 @@
 #!/bin/env python3
 
 import os
+import sys
 
 import ant_list
 import ant_idx
 import ants_db
 import ant_opt
+import ant_arg
 
 if __name__ == '__main__':
 
-    example_hill=("hill1","test_data/hill1")
+    argv0 = ['./ant_arg.py',
+            #'-v',
+            #'-d', '512',
+            '-d', '1K',
+            #'-d', '64K',
+            #'-d', '1M',
+            '-1',
+            '-2',
+            'test_data/hill1'
+            ]
 
-    #head_size = ant_opt.head_size_to_int("512")
-    head_size = ant_opt.head_size_to_int("1K")
-    #head_size = ant_opt.head_size_to_int("64K")
-    #head_size = ant_opt.head_size_to_int("1M")
-    #head_size = ant_opt.head_size_to_int("8M")
-    #head_size = ant_opt.head_size_to_int("256M")
-    ant_opt.set_verbose(False)
+    mydebug = True
+    mydebug = False
+
+    if mydebug:
+        argv = argv0[1:]
+    else:
+        argv = sys.argv[1:]
+
+    args = ant_arg.ant_arg(argv)
+    #print (args)
+
+    example_hill=("hill1",args.path)
+
+    head_size = ant_opt.head_size_to_int(args.head_sz)
+    ant_opt.set_verbose(args.verbose)
 
     hill_name,hill_path = example_hill
 
     ants_db.add_hill(hill_name,hill_path)
 
     for base_dir, file_name in ant_list.hill_elems(hill_path):
-        sz,h,a = ant_idx.create_ant_idx(base_dir,file_name,is_head=True,head_size=head_size,is_all=True)
+        sz,h,a = ant_idx.create_ant_idx(base_dir,file_name,is_head=args.l1,head_size=head_size,is_all=args.l2)
         # hss = head_size_to_str(head_size)
         # print(f"{sz:12} {hss:>4} {h} {a} {base_dir}/{file_name}")
         ants_db.hill_add_ant(hill_name,sz,head_size,h,a,base_dir+"/"+file_name)
 
     ants_db.ants_view(hill_name)
-    ants_db.ants_pprint()
+    if(ant_opt.is_verbose()):
+        ants_db.ants_pprint()
